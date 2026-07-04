9f335ebc5c83bc73524108e844c9d79cb24bda14
Marcos <Reimarcosneto3@gmail.com>
2026-07-04 09:15:51 -0300

    fix: player move 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,56 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./index.css";
 import movim from "./scripts/index.js";
 import { Map } from "./scenes/nuvema_town/Map";
 import { Player } from "./components/Player";
 
 function App() {
+  const cameraRef = useRef(null);
+
   const [mapDimensions, setMapDimensions] = useState({
     widthPx: 0,
     heightPx: 0,
   });
-  const [characterPosition, setCharacterPosition] = useState({ x: 14, y: 15 });
 
-  // Use a state or ref to track viewport if it needs to update on resize
-  const [viewport, setViewport] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+  const [characterPosition, setCharacterPosition] = useState({
+    x: 14,
+    y: 15,
   });
-
-  const TILE_SIZE = 16;
-  const SCALE = 6;
-
-  // Calculate visual positions outside of useEffect so they are available to JSX
-  const visualX = Math.max(
-    Math.min(
-      -(characterPosition.x * TILE_SIZE - viewport.width / (2 * SCALE)),
-      0,
-    ),
-    -(mapDimensions.widthPx - viewport.width / SCALE),
-  );
-
-  const visualY = Math.max(
-    Math.min(
-      -(characterPosition.y * TILE_SIZE - viewport.height / (2 * SCALE)),
-      0,
-    ),
-    -(mapDimensions.heightPx - viewport.height / SCALE),
-  );
-
+    const [cameraSize, setCameraSize] = useState({
+    width: 0,
+    height:0,
+  });
   useEffect(() => {
-    movim(setCharacterPosition);
+    if (cameraRef.current){
+      setCameraSize({
+        width: cameraRef.current.clientWidth,
+        height:cameraRef.current.clientHeight,
+      })
+    }
   }, []);
 
+  // Use a state or ref to track viewport if it needs to update on resize
+
+  const TILE_SIZE = 16;
+  const SCALE = 3;
+
+  // Calculate visual positions outside of useEffect so they are available to JSX
+  const visualX = Math.max(Math.min(-(characterPosition.x * TILE_SIZE +TILE_SIZE / 2 -cameraSize.width / (2 * SCALE)),0),-(mapDimensions.widthPx - cameraSize.width / SCALE));
+
+  const visualY = Math.max(Math.min(-(characterPosition.y * TILE_SIZE +TILE_SIZE / 2 -cameraSize.height / (2 * SCALE)),0),
+  -(mapDimensions.heightPx - cameraSize.height / SCALE));
+
+  useEffect(() => {movim(setCharacterPosition);}, []);
+
   return (
-    <div className="GameCamera">
+    <div className="GameCamera" ref={cameraRef}>
       <div
         className="World"
         style={{
           width: `${mapDimensions.widthPx}px`,
           height: `${mapDimensions.heightPx}px`,
           // Ensure you use the variables calculated above
-          transform: `scale(${SCALE}) translate3d(${visualX - 45}px, ${visualY - 10}px, 0)`,
+          transform: `scale(${SCALE}) translate3d(${visualX}px, ${visualY}px, 0)`,
           transformOrigin: "top left",
         }}
       >
