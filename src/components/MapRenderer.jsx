6f58f1dae1f36365d44b365b30361ea1baa30aeb
junior010101 <marcondesjunior2209@gmail.com>
2026-07-04 23:42:47 -0300

    add: logica de camadas

--- src/components/MapRenderer.jsx
+++ src/components/MapRenderer.jsx
@@ -7,79 +7,80 @@ export const MapRenderer = ({ mapData, tileSize }) => {
 
   return (
     <>
-      <div
-        className="SceneMap"
-        style={{
-          gridTemplateColumns: `repeat(${colunasDoMapa}, ${tamanhoDoBloco}px)`,
-          gridTemplateRows: `repeat(${linhasDoMapa}, ${tamanhoDoBloco}px)`,
-          width: `${colunasDoMapa * tamanhoDoBloco}px`,
-          height: `${linhasDoMapa * tamanhoDoBloco}px`,
-        }}
-      >
-        {mapData.layers.map((layerE, layerI) => {
-          const layer = layerI + 1;
+      {mapData.layers.map((layerE, layerI) => {
+        return (
+          <div
+            key={layerI}
+            className="MapLayer"
+            style={{
+              width: `${colunasDoMapa * tamanhoDoBloco}px`,
+              height: `${linhasDoMapa * tamanhoDoBloco}px`,
 
-          return layerE.data.map((data, index) => {
-            const column = (index % colunasDoMapa) + 1;
-            const row = Math.floor(index / colunasDoMapa) + 1;
+              gridTemplateColumns: `repeat(${colunasDoMapa}, ${tamanhoDoBloco}px)`,
+              gridTemplateRows: `repeat(${linhasDoMapa}, ${tamanhoDoBloco}px)`,
+              zIndex: (layerI + 1) * 10,
+            }}
+          >
+            {layerE.data.map((data, index) => {
+              const column = (index % colunasDoMapa) + 1;
+              const row = Math.floor(index / colunasDoMapa) + 1;
 
-            let spriteSheetColumns;
-            let spriteSheetUrl;
-            let transformStyle = "";
+              let spriteSheetColumns;
+              let spriteSheetUrl;
+              let transformStyle = "";
 
-            if (data == 0) {
+              if (data == 0) {
+                return (
+                  <div
+                    key={index}
+                    style={{
+                      backgroundColor: "transparent",
+                      gridColumnStart: column,
+                      gridRowStart: row,
+                    }}
+                  ></div>
+                );
+              }
+
+              mapData.tilesets.forEach((options) => {
+                if (data >= options.firstgid) {
+                  spriteSheetColumns = options.columns;
+                  spriteSheetUrl = options.image;
+                }
+              });
+
+              const realData = data & 0xffff;
+
+              const flipHorizontal = (data & 0x80000000) !== 0;
+              const flipVertical = (data & 0x40000000) !== 0;
+              const flipDiagonal = (data & 0x20000000) !== 0;
+
+              if (flipHorizontal) transformStyle += "scaleX(-1) ";
+              if (flipVertical) transformStyle += "scaleY(-1) ";
+              if (flipDiagonal) transformStyle += "rotate(-90deg) scaleX(-1) ";
+
+              const posX = ((realData % spriteSheetColumns) - 1) * 16;
+              const posY = Math.floor(realData / spriteSheetColumns) * 16;
+
               return (
                 <div
                   key={index}
                   style={{
-                    zIndex: layer,
-                    backgroundColor: "transparent",
+                    backgroundImage: `url(${spriteSheetUrl})`,
+                    imageRendering: "pixelated",
+                    backgroundPosition: `-${posX}px -${posY}px`,
+                    transformOrigin: "center",
+                    transform: transformStyle.trim(),
+                    backgroundRepeat: "no-repeat",
                     gridColumnStart: column,
                     gridRowStart: row,
                   }}
                 ></div>
               );
-            }
-
-            mapData.tilesets.forEach((options) => {
-              if (data >= options.firstgid) {
-                spriteSheetColumns = options.columns;
-                spriteSheetUrl = options.image;
-              }
-            });
-
-            const realData = data & 0xffff;
-
-            const flipHorizontal = (data & 0x80000000) !== 0;
-            const flipVertical = (data & 0x40000000) !== 0;
-            const flipDiagonal = (data & 0x20000000) !== 0;
-
-            if (flipHorizontal) transformStyle += "scaleX(-1) ";
-            if (flipVertical) transformStyle += "scaleY(-1) ";
-            if (flipDiagonal) transformStyle += "rotate(-90deg) scaleX(-1) ";
-
-            const posX = ((realData % spriteSheetColumns) - 1) * 16;
-            const posY = Math.floor(realData / spriteSheetColumns) * 16;
-
-            return (
-              <div
-                key={index}
-                style={{
-                  zIndex: layer,
-                  backgroundImage: `url(${spriteSheetUrl})`,
-                  imageRendering: "pixelated",
-                  backgroundPosition: `-${posX}px -${posY}px`,
-                  transformOrigin: "center",
-                  transform: transformStyle.trim(),
-                  backgroundRepeat: "no-repeat",
-                  gridColumnStart: column,
-                  gridRowStart: row,
-                }}
-              ></div>
-            );
-          });
-        })}
-      </div>
+            })}
+          </div>
+        );
+      })}
     </>
   );
 };
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,13 +6,12 @@ export const Player = ({ posX, posY, tileSize }) => {
   const offsetY = tileSize;
 
   return (
-    <>
-      <div
-        className="PlayerContainer"
-        style={{
-          transform: `translate3d(${visualX}px, ${visualY - offsetY}px, 0)`,
-        }}
-      />
-    </>
+    <div
+      className="PlayerContainer"
+      style={{
+        transform: `translate3d(${visualX}px, ${visualY - offsetY}px, 0)`,
+        zIndex: 25,
+      }}
+    />
   );
 };
