ec8f652eb77b70c6781eaa2081db37f07f7836fc
junior010101 <marcondesjunior2209@gmail.com>
2026-07-04 21:04:36 -0300

    add: hitbox map

--- src/scenes/nuvema_town/hitbox.js
+++ src/scenes/nuvema_town/hitbox.js
@@ -1,10 +1,32 @@
 const hitbox = Array.from({ length: 30 }, () => Array(30).fill(0));
 const lista_de_blocos_com_colisão = [
-    {x:0,y:0},{x:1,y:27},{x:0,y:0},{x:0,y:27},{x:1,y:27},{x:2,y:27},{x:3,y:26},{x:4,y:27}]
-lista_de_blocos_com_colisão.forEach(element => {
-hitbox[element.y][element.x] = 1
-}); 
+  { x: 4, y: 1 },
+  { x: 14, y: 3 },
+  { x: 14, y: 4 },
+  { x: 15, y: 4 },
+];
 
+for (let i = 2; i < 30; i++) {
+  lista_de_blocos_com_colisão.push({ x: 3, y: i });
+}
+for (let i = 4; i < 30; i++) {
+  lista_de_blocos_com_colisão.push({ x: i, y: 29 });
+}
 
+for (let i = 0; i < 30; i++) {
+  lista_de_blocos_com_colisão.push({ x: 24, y: i });
+}
 
-export default hitbox
+for (let i = 5; i < 14; i++) {
+  lista_de_blocos_com_colisão.push({ x: i, y: 2 });
+}
+
+for (let i = 0; i < 7; i++) {
+  lista_de_blocos_com_colisão.push({ x: 16, y: i });
+}
+
+lista_de_blocos_com_colisão.forEach((element) => {
+  hitbox[element.y][element.x] = 1;
+});
+
+export default hitbox;
